5f7ffe01fe6fd385190df24ff25e71ec489e9e38
Matt Pusch <Matt Pusch@10.13.165.169>
2017-03-20 16:43:23 -0600

    everything but maps - monday march 20

--- src/com/RESTSample/HelloWorld.java
+++ src/com/RESTSample/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.RESTSample;
 
 import java.io.InputStream;
+import java.util.Objects;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,7 +18,31 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 @Path("/")
 public class HelloWorld 
-{	
+{
+	// NECESSARY??
+    private final String APIKEY = "AIzaSyDAIN1a6W6Hz1DdusqJtuVq1PLnYNn3WV8";
+
+    // BROKEN!!
+    private JSONArray createJSONArrayfromText(String action)
+    {
+        try 
+        {
+            HttpResponse<JsonNode> response = 
+            		Unirest.get("https://api.auroras.live/v1/" + action)
+                    .header("accept", "application/json")
+                    .asJson();
+            if (jsonResponse.getBody().isArray()){
+                return jsonResponse.getBody().getArray();
+            }else{
+                throw new UnirestException("Should not be object");
+            }
+
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
 	@Path("type=locations")
 	@GET
 	@Produces("application/json")
@@ -69,6 +95,10 @@ public class HelloWorld
 		return Response.status(200).entity(object).build();
 	}
 	
+	
+	
+	// IMAGES CURRENTLY FAIL TEST 22 - MUST BE ABLE TO RETURN JSON
+	
 	@Path("type=images&image={img}")
 	@GET
 	@Produces("image/jpeg")
@@ -96,5 +126,133 @@ public class HelloWorld
 			String att = "Powered by Aurora.live";
 			jsonObject.put("Attribution", att);
 			return Response.status(200).entity(response.getBody().toString()).build();
+	}
+	
+	
+	
+	// ARCHIVE FAILS TEST 17 - (POSSIBLY IGNORING % IN URL)
+	
+	@Path("type=archive{action}")
+	@GET
+	@Produces("application/json")
+	public Response getArchive(@PathParam("action") String action) throws JSONException, UnirestException
+	{
+			JSONObject jsonObject = new JSONObject();
+			HttpResponse<JsonNode> response = 
+					Unirest.get("http://api.auroras.live/v1/?type=archive" + action)
+					.header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
+			
+			jsonObject = response.getBody().getObject();
+			String att = "Powered by Aurora.live";
+			jsonObject.put("Attribution", att);
+			return Response.status(200).entity(response.getBody().toString()).build();
+	}
+	
+	@Path("type=weather{action}")
+	@GET
+	@Produces("application/json")
+	public Response getWeather(@PathParam("action") String action) throws JSONException, UnirestException
+	{
+		JSONObject jsonObject = new JSONObject();
+		HttpResponse<JsonNode> response = 
+			Unirest.get("http://api.auroras.live/v1/?type=weather" + action)
+			.header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
+		
+		jsonObject = response.getBody().getObject();
+		String att = "Powered by Aurora.live";
+		jsonObject.put("Attribution", att);
+		return Response.status(200).entity(response.getBody().toString()).build();
+	}
+	
+	@Path("type=all{action}")
+	@GET
+	@Produces("application/json")
+	public Response getAll(@PathParam("action") String action) throws JSONException, UnirestException
+	{
+			JSONObject jsonObject = new JSONObject();
+			HttpResponse<JsonNode> response = 
+					Unirest.get("http://api.auroras.live/v1/?type=all" + action)
+					.header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
+			
+			jsonObject = response.getBody().getObject();
+			String att = "Powered by Aurora.live";
+			jsonObject.put("Attribution", att);
+			return Response.status(200).entity(response.getBody().toString()).build();
+	}
+    
+	
+	
+	// BROKEN!!
+	
+	@Path("type=map{action}")
+	@GET
+	@Produces()
+	public Response getMap(@PathParam("action") String action) throws JSONException, UnirestException
+	{
+		JSONArray json = createJSONArrayfromText();
+		JSONObject jsonObject = new JSONObject();
+		boolean found = false;
+		
+        for (int i = 0; i < json.length(); i++)
+        {
+            JSONObject j = json.getJSONObject(i);
+            if (Objects.equals(j.getString("id"), action)) // ???
+            {
+                found = true;
+                jsonObject = j;
+            }
+        }
+        
+        if (!found){
+            System.out.println("request not in auroras.live repo");
+        }
+        
+        else	//ID Tag found, let us make the call to google maps api
+        {
+            String request = "";
+            String longCoor = jsonObject.getString("long");
+            String latCoor = jsonObject.getString("lat");
+
+            try { //Call google maps api (Currently returning png, can change to whatever works best.
+                String address = "https://maps.googleapis.com/maps/api/staticmap?center="+latCoor+","+longCoor+"&zoom=13&size=600x300&format=png&key=" + APIKEY;
+                HttpResponse mapResponse = Unirest.get(address)
+                        .header("accept", "application/json")
+                        .asBinary();
+                //Contains raw binary data.
+                InputStream is = mapResponse.getRawBody();
+                //Currently storing as picture, return as picture (Similar to parseImage or parseEmbed)
+                File f = new File("test.png");
+                Files.copy(is, f.toPath());
+            } catch (UnirestException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+		/*
+		HttpResponse<JsonNode> response = 
+				Unirest.get("http://api.auroras.live/v1/?" + action)
+				.header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
+		
+		jsonObject = response.getBody().getObject();
+		String att = "Powered by Aurora.live";
+		jsonObject.put("Attribution", att);
+		return Response.status(200).entity(response.getBody().toString()).build();
+		*/
+	}
+	
+	@Path("{failed}")
+	@GET
+	@Produces("application/json")
+	public Response failedRequest(@PathParam("failed") String failed) throws JSONException, UnirestException
+	{
+		JSONObject jsonObject = new JSONObject();
+		HttpResponse<JsonNode> response = 
+				Unirest.get("http://api.auroras.live/v1/?" + failed)
+				.header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
+		
+		jsonObject = response.getBody().getObject();
+		String att = "Powered by Aurora.live";
+		jsonObject.put("Attribution", att);
+		return Response.status(200).entity(response.getBody().toString()).build();
 	}
 }
